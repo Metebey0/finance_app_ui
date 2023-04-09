eebd0784b2f7b5789fad003b3338cfa4e7082409
Metebey0 <beyaz-mehmet-t@outlook.com>
2023-04-10 01:11:25 +0300

    commit

---
+++ lib/models/transactions.dart
@@ -0,0 +1,39 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+import 'package:finance_app/colors.dart' as color;
+
+class Transaction {
+  String title;
+  String amount;
+  String date;
+  IconData iconData;
+  Color color;
+  Transaction(
+    this.title,
+    this.amount,
+    this.date,
+    this.iconData,
+    this.color,
+  );
+}
+
+List<Transaction> transaction() {
+  return [
+    Transaction("Food", "\$450", "8 April 2023", Icons.fastfood_rounded,
+        color.Colors.orangeColor),
+    Transaction("Medicine", "\$4500", "9 April 2023",
+        Icons.medical_information_rounded, color.Colors.greenColor),
+    Transaction("Clothes", "\$45", "11 April 2023", Icons.shop,
+        color.Colors.accentColor),
+    Transaction("House", "\$45050", "12 April 2023", Icons.house_rounded,
+        randomColor()),
+    Transaction("Car Rental", "\$1650", "12 April 2023",
+        Icons.car_rental_rounded, randomColor()),
+  ];
+}
+
+Color randomColor() {
+  return Color(Random().nextInt(0xFFFFFFFF)).withAlpha(0xFF);
+}
